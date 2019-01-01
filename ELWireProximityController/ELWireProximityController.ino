8b9f65a36c6ba63712bf6b11fc03ba17e24570d4
Craig Hollabaugh <holla2040@gmail.com>
2018-12-31 22:08:35 -0700

    working version with powersave (1.4mA idle), 1 second check interval, no serial printing

--- ELWireProximityController/ELWireProximityController.ino
+++ ELWireProximityController/ELWireProximityController.ino
@@ -5,30 +5,31 @@
 
 // distance in inches
 
+#define POWERSAVE 
+// #define DEBUG
+
+#ifdef POWERSAVE
 #include "LowPower.h"
+#endif
 
 const int LED             = 13; 
 const int trigPin         = 12; 
 const int echoPin         = 11; 
-const int hvEnablePin     = 10;
+const int hvEnablePin     = 4;
 const int sensorPowerPin1 =  9;
 const int sensorPowerPin2 =  8;
 const int sensorPowerPin3 =  7;
 const int sensorPowerPin4 =  6;
 
-const int distanceLimit   = 30;
-const int positiveHitLimit= 10;
+const int distanceLimit   = 50;
 
 long microsecondsToInches(long microseconds) {
-   return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
+   return (microseconds/2) / 74;
 }
 
 void sensorPower(bool e) {
   if (e) {
+    pinMode(echoPin,INPUT_PULLUP); 
     pinMode(sensorPowerPin1,OUTPUT); 
     pinMode(sensorPowerPin2,OUTPUT); 
     pinMode(sensorPowerPin3,OUTPUT); 
@@ -38,16 +39,28 @@ void sensorPower(bool e) {
     digitalWrite(sensorPowerPin3,HIGH);
     digitalWrite(sensorPowerPin4,HIGH);
   } else {
+    pinMode(echoPin,OUTPUT); 
+    digitalWrite(echoPin, LOW);
+    digitalWrite(trigPin, LOW);
+    digitalWrite(sensorPowerPin1,LOW);
+    digitalWrite(sensorPowerPin2,LOW);
+    digitalWrite(sensorPowerPin3,LOW);
+    digitalWrite(sensorPowerPin4,LOW);
+/*
     pinMode(sensorPowerPin1,INPUT);
     pinMode(sensorPowerPin2,INPUT);
     pinMode(sensorPowerPin3,INPUT);
     pinMode(sensorPowerPin4,INPUT);
+*/
   }
 }
 
 void setup() {
+#ifdef DEBUG
   Serial.begin(115200);
-  Serial.println("ELWireProximityController setup");
+  delay(10);
+  Serial.println("\nELWireProximityController setup");
+#endif
 
   pinMode(trigPin,        OUTPUT); 
   pinMode(echoPin,        INPUT_PULLUP); 
@@ -59,52 +72,96 @@ void setup() {
 
   sensorPower(true);
 
-  //LowPower.idle(SLEEP_2S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+#ifdef POWERSAVE
+  LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+#endif
 }
 
-bool inRange() {
-  // looking for positiveHitLimit
-  int positives = 0;
-  long duration, distance;
 
-  for (int i = 0; i < positiveHitLimit+2; i++) {
+
+long getDuration() {
+    const int maxdur = 20000;
+    long duration;
     digitalWrite(trigPin, LOW);
     delayMicroseconds(5);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = microsecondsToInches(duration);
+    duration =  pulseIn(echoPin, HIGH, maxdur);
+    if (duration == 0) duration = maxdur;
+#ifdef DEBUG
+    Serial.println(duration);
+#endif
+    delay(15); // this is needed for sensor to reset
+    return duration;
+}
 
-/*
-    Serial.print(duration);
-    Serial.print(" ");
-*/
-    Serial.println(distance);
 
-    if (distance < distanceLimit) {
-      positives++;
-      if (positives > positiveHitLimit) return true;
-    }
+bool inRange() {
+  long distance,duration = 0;
+  const int avecount = 5;
+
+#ifdef POWERSAVE
+  sensorPower(true);
+  delay(200); 
+#endif
+
+  // pitching the first 2 after power on
+  getDuration();
+  getDuration();
+
+  for (int i = 0; i < avecount; i++) {
+    long v;
+    v = getDuration();
+    duration += v;
+  }
+
+#ifdef POWERSAVE
+  sensorPower(false);
+#endif
+
+#ifdef DEBUG
+  Serial.println(duration);
+  Serial.println();
+#endif
+
+  duration /= avecount;
+  distance = microsecondsToInches(duration);
+
+#ifdef DEBUG
+  Serial.print(distance);
+  Serial.print(" ");
+  Serial.println(float(distance)/12.0);
+#endif
+
+  if (distance < distanceLimit) {
+    return true;
   }
   return false;
 }
 
 void loop() {
-  sensorPower(true);
-  delay(200);
   if (inRange()) {
+#ifdef DEBUG
     Serial.println("IN"); 
+#endif
     digitalWrite(LED,HIGH); // LED ON 
   } else {
+#ifdef DEBUG
     Serial.println("OUT"); 
-    digitalWrite(LED,LOW); // LED OFF
+#endif
+    digitalWrite(LED,HIGH); // quick blink
+    delay(1);
+    digitalWrite(LED,LOW); 
   }
-  sensorPower(false);
 
-  delay(2000);
 
-  //LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);  
-  //delay(100);
+#ifdef POWERSAVE
+  delay(100); // serial flush time
+  // LowPower.idle(SLEEP_15MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+  LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);  
+#else
+  delay(1000);
+#endif
 }
 
